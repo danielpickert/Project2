ea4bb119eae91a24d115a16196a5fbdcf7981ea2
Daniel Pickert <dpickert1@gmail.com>
2018-06-11 20:28:51 -0400

    added cover photo and title to home page

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const routes = require("./routes")
 const path = require("path")
 app.use(express.static(path.join(__dirname,"public")))
 
+
 app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
+
+
 // home
 app.get("/", routes.home) 
 
